94f3245128cd8b8a95517afecb79f6c07c025fb9
youngjoo9992 <71207893+youngjoo9992@users.noreply.github.com>
2021-08-08 14:46:31 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+
+    public Vector2[] playerCoordinate = { new Vector2(-1.34375f, 3.6875f), new Vector2(1.34375f, 3.6875f), new Vector2(-1.34375f, 1.3125f), new Vector2(1.34375f, 1.3125f) };
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void button1Click()
+    {
+        player.transform.position = playerCoordinate[0];
+    }
+
+    public void button2Click()
+    {
+        player.transform.position = playerCoordinate[1];
+    }
+
+    public void button3Click()
+    {
+        player.transform.position = playerCoordinate[2];
+    }
+
+    public void button4Click()
+    {
+        player.transform.position = playerCoordinate[3];
+    }
+
+}
