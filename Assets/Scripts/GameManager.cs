81f842368b6ae661c7b5838ec159f5c692dd4d6d
youngjoo9992 <71207893+youngjoo9992@users.noreply.github.com>
2021-08-08 16:55:14 +0900

    플레이어 모션 추가(임시)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private int destination;//0 = 1, 1 = 2, 2 = 3, 3 = 4
+
+    public float playerSpeed;
+
     public GameObject player;
 
     public Vector2[] playerCoordinate = { new Vector2(-1.34375f, 3.6875f), new Vector2(1.34375f, 3.6875f), new Vector2(-1.34375f, 1.3125f), new Vector2(1.34375f, 1.3125f) };
@@ -15,27 +21,44 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        gameScene();
+    }
+    
+    private void gameScene()
+    {
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            movePlayer();
+        }
+    }
+
+    private void movePlayer()
+    {
+        player.transform.position = Vector3.Lerp(player.transform.position, playerCoordinate[destination], playerSpeed);
     }
 
     public void button1Click()
     {
-        player.transform.position = playerCoordinate[0];
+        //player.transform.position = Vector3.Lerp(player.transform.position, playerCoordinate[0], playerSpeed);
+        destination = 0;
     }
 
     public void button2Click()
     {
-        player.transform.position = playerCoordinate[1];
+        //player.transform.position = playerCoordinate[1];
+        destination = 1;
     }
 
     public void button3Click()
     {
-        player.transform.position = playerCoordinate[2];
+        //player.transform.position = playerCoordinate[2];
+        destination = 2;
     }
 
     public void button4Click()
     {
-        player.transform.position = playerCoordinate[3];
+        //player.transform.position = playerCoordinate[3];
+        destination = 3;
     }
 
 }
