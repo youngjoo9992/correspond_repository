e01eedf5b1e8e885c0f43ddd4d743a9a1590d3ef
youngjoo9992 <71207893+youngjoo9992@users.noreply.github.com>
2021-08-09 12:44:57 +0900

    UI 배치 임시저장

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,13 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private int destination;//0 = 1번 구역, 1 = 2번 구역, 2 = 3번 구역, 3 = 4번 구역
+    private int gold;//골드
 
+    private float musicVolume;//음악 볼륨
+    private float soundEffectVolume;//효과음 볼륨
     private float playerSpeed = 0.3f;//플레이어 이동속도
 
-    public GameObject player;//플레이어 게임오브젝트
+    private GameObject player;//플레이어 게임오브젝트
 
     //구역별 플레이어 좌표
     public Vector2[] playerCoordinate = { new Vector2(-1.34375f, 3.6875f), new Vector2(1.34375f, 3.6875f), new Vector2(-1.34375f, 1.3125f), new Vector2(1.34375f, 1.3125f) };
@@ -95,6 +98,7 @@ public class GameManager : MonoBehaviour
     //플레이어 지정 좌표 이동 함수 - 윤영주
     private void movePlayer()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         player.transform.position = Vector3.Lerp(player.transform.position, playerCoordinate[destination], playerSpeed);
     }
 
@@ -120,6 +124,24 @@ public class GameManager : MonoBehaviour
     public void button4Click()
     {
         destination = 3;
+    }
+
+    //StartButton 클릭 함수
+    public void startButtonClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    //ShopButton 클릭 함수
+    public void shopButtonClick()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    //MenuButton 클릭 함수
+    public void menuButtonClick()
+    {
+        SceneManager.LoadScene("Menu");
     }
 
 }
